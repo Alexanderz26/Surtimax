426999ab93b54159ffbca818b8f646368c670d0a
Alexanderz26 <gchalapud@misena.edu.co>
2022-10-23 19:16:59 -0500

    login,adm
    login,adm

--- main/urls.py
+++ main/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from main.views import inicioAdmin
+from main.views import inicio, inicioAdmin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', inicio , name="inicio"),
     path('adm/', inicioAdmin , name="inicio-admin"),
     path('usuarios/', include('usuarios.urls')),
 
--- main/views.py
+++ main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+def inicio(request):
+    titulo="Inicio"
+    context = {  
+         'titulo': titulo 
+    }
+    return render(request,'login.html',context)
+
 def inicioAdmin(request):
-   
-    context = {     
+    titulo="Inicio"
+    context = {  
+        'titulo': titulo   
     }
     return render(request,'index.html',context)
