13c74b075c6c8442ff79ee2d92ad31e901183ef4
yefer2002 <110873793+yefer2002@users.noreply.github.com>
2022-10-26 22:50:40 -0500

    cree el modelo de productos

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,42 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 # Create your models here.
+class Marca(models.Model):
+        nombre=models.CharField(max_length=45,verbose_name="Nombre")
+class Estado(models.Model):
+        ACTIVO ="1",_('ACTIVO')
+        INACTIVO ="0",_('INACTIVO')
+        Estado =models.CharField(max_length=1, choice=Estado.choice,default=Estado.ACTIVO,verbose_name="Estado")
+
+
+class Categoria(models.Model):
+       nombre=models.CharField(max_length=50,verbose_name="Nombre")
+       descripcion=models.CharField(max_length=256,verbose_name="Descripción")
+class Estado(models.Model):
+        ACTIVO ="1",_('ACTIVO')
+        INACTIVO ="0",_('INACTIVO')
+        estado =models.CharField(max_length=1, choice=Estado.choice,default=Estado.ACTIVO,verbose_name="Estado")
+
+   
+
+class Presentacion(models.Model):
+        nombre=models.CharField(max_length=50,verbose_name="Nombre")
+        prefijos=models.CharField(max_length=5,verbose_name="Prefijos")
+class Estado(models.Model):
+        ACTIVO ="1",_('ACTIVO')
+        INACTIVO ="0",_('INACTIVO')
+        estado =models.CharField(max_length=1, choice=Estado.choice,default=Estado.ACTIVO,verbose_name="Estado")
+
+ 
+class Productos(models.Model):
+     categoria=models.ForeignKey(Categoria,on_delete=models.CASCADE,verbose_name="Categorias")
+     marca=models.ForeignKey(Marca,on_delete=models.CASCADE,verbose_name="Marca")
+     presentacion=models.ForeignKey(Presentacion,on_delete=models.CASCADE,verbose_name="Presentación")
+     codigoBarras=models.CharField(max_length=50,verbose_name="CODBARRAS")
+     nombre=models.CharField(max_length=50,verbose_name="Nombre")
+     descripcion=models.CharField(max_length=1024,verbose_name="Descripción")
+class Estado(models.Model):
+        ACTIVO ="1",_('ACTIVO')
+        INACTIVO ="0",_('INACTIVO')
+        estado =models.CharField(max_length=1, choice=Estado.choice,default=Estado.ACTIVO,verbose_name="Estado")
