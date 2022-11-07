4500819341a8fcadad4310a1cbf9f5b49a35a578
Alexanderz26 <gchalapud@misena.edu.co>
2022-11-06 21:29:33 -0500

    de crear usuarios y base global de USUARIOS
    se creo usuario base y Crear usuarios falta botones y enlazar estilos

--- main/settings.py
+++ main/settings.py
@@ -39,8 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
+    'crispy_forms', #pip install crispy-forms
+    'crispy_bootstrap5',#pip install crispy-bootstrap5
     'usuarios',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
