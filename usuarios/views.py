8e4813703543aec01b619ac121e518f3bc23f281
Alexanderz26 <gchalapud@misena.edu.co>
2022-11-14 15:02:00 -0500

    botones de editar y eliminar funcional
    funcionan botones de editar y eliminar

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,10 @@
 from multiprocessing import context
+
 from django.shortcuts import redirect, render
 
 from usuarios.forms import UsuarioForm
 from usuarios.models import Usuario
+
 
 # Create your views here.
 def usuarios (request):
@@ -29,5 +31,41 @@ def usuarios_crear(request):
     context={
         'titulo': titulo,
         'form': form
+    }
+    return render(request, 'usuarios/usuarios-crear.html', context)
+
+# Crear Editar.
+def usuarios_editar(request, pk):
+    titulo="Usuarios - Editar"
+    usuario=Usuario.objects.get(id=pk)
+    if request.method == "POST":
+        form= UsuarioForm(request.POST,instance=usuario)
+        if form.is_valid():
+            form.save()
+            return redirect ('usuarios')
+        else:
+            print ("Error al guardar")
+    else:
+        form= UsuarioForm(instance=usuario)    
+
+    context={
+        'titulo': titulo,
+        'form': form
+    }
+    return render(request, 'usuarios/usuarios-crear.html', context)
+
+# Crear Eliminar.
+def usuarios_eliminar(request, pk):
+    titulo="Usuarios - Eliminar"
+    usuarios= Usuario.objects.all()
+
+    Usuario.objects.filter(id=pk).update(
+            estado='0'
+        )  
+    return redirect('usuarios')
+
+    context={
+        'titulo': titulo,
+        'usuarios': usuarios,
     }
     return render(request, 'usuarios/usuarios-crear.html', context)
