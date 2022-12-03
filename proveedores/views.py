068aae2927c0a3c7fa135c36eb2516280dcfaf09
GeoLeont <galeon18@misena.edu.co>
2022-12-03 18:45:48 -0500

    Agrega tabla a vista de proveedores. consume datos desde la DDBB . DataTables agredado con buscador y botones para descargar reportes.

--- proveedores/views.py
+++ proveedores/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 from proveedores.models import Proveedor
 # Create your views here.
 
 def proveedores (request):
     #importar los proveedores desde el modulo admin
-    proveedores=Proveedor.objects.all
+    proveedores_list=Proveedor.objects.all()
 
-    return render(request,'proveedores/proveedores.html', {"proveedores": proveedores})
+    return render(request,'proveedores/proveedores.html', {"proveedores": proveedores_list})
